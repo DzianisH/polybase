a5e59cdd70e6f05f901618669dbca30cccd8020b
DzianisH <gavrilovetsden@gmail.com>
2019-11-21 02:17:23 +0300

    Updates

---
+++ precache-manifest.017b2dfc5f12845cf6ffb38fae318ade.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf7b88670a02148e1f140dd3b549cda9",
+    "url": "/polybase-demo/index.html"
+  },
+  {
+    "revision": "cd654154f55c4d6905b9",
+    "url": "/polybase-demo/static/css/main.8a6f41a8.chunk.css"
+  },
+  {
+    "revision": "10e7d218e03e0643e90e",
+    "url": "/polybase-demo/static/js/2.e6de4e4b.chunk.js"
+  },
+  {
+    "revision": "cd654154f55c4d6905b9",
+    "url": "/polybase-demo/static/js/main.e1c7bed9.chunk.js"
+  },
+  {
+    "revision": "66788d870c987a8b71d0",
+    "url": "/polybase-demo/static/js/runtime-main.e8f3f82b.js"
+  }
+]);
