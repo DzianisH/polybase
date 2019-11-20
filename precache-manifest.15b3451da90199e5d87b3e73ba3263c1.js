090a56d50955ab67d6b425a2c8273195562dff04
DzianisH <gavrilovetsden@gmail.com>
2019-11-21 02:20:03 +0300

    Updates

---
+++ precache-manifest.15b3451da90199e5d87b3e73ba3263c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5da8cb473ebc9659d8dec969fd99c2d",
+    "url": "/polybase/index.html"
+  },
+  {
+    "revision": "33260d753aef14386115",
+    "url": "/polybase/static/css/main.8a6f41a8.chunk.css"
+  },
+  {
+    "revision": "10e7d218e03e0643e90e",
+    "url": "/polybase/static/js/2.e6de4e4b.chunk.js"
+  },
+  {
+    "revision": "33260d753aef14386115",
+    "url": "/polybase/static/js/main.2f39f89d.chunk.js"
+  },
+  {
+    "revision": "9556e167391b82460278",
+    "url": "/polybase/static/js/runtime-main.5a55a60d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/polybase-demo/precache-manifest.017b2dfc5f12845cf6ffb38fae318ade.js"
+  "/polybase/precache-manifest.15b3451da90199e5d87b3e73ba3263c1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/polybase-demo/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/polybase/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
